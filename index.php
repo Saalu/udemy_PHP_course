acc8a89ce214b528c095216a84a225d9f014742c
unknown <saaluissaka>
2020-10-29 20:52:22 +0000

    learning PHP basics

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+define('NAME', 'mario');
+
+$x= "objective";
+
+echo "My first PHP file" . " ". $x; 
+// echo $x;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>PHP Course</title>
+</head>
+<body>
+
+<h1><?php echo "Hello, from Saalu " ?> </h1>
+    <h1><?php echo NAME ?> </h1>
+    
+</body>
+</html>
